595b2b56e81c3674169925ee740d5015080534f2
Joakim Hansson <joakim.hansson@tretton37.com>
2017-09-27 16:51:44 +0200

    Hello, Logging!

---
+++ Loggers/SentryLogger.cs
@@ -0,0 +1,41 @@
+﻿#if NETSTANDARD2_0
+using Microsoft.Extensions.Logging;
+#endif
+
+using SharpRaven.Data;
+using System;
+
+namespace ServiceFabric.Utils.Logging.Loggers
+{
+
+    public class SentryLogger : ILogger
+    {
+        private readonly SentryLoggerConfiguration _configuration;
+
+        public SentryLogger(SentryLoggerConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel == LogLevel.Critical;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+
+            _configuration.RavenClient.Capture(new SentryEvent(exception));
+        }
+    }
+}
---
+++ SentryLoggerConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using SharpRaven;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceFabric.Utils.Logging
+{
+    public class SentryLoggerConfiguration
+    {
+        public IRavenClient RavenClient { get; set; }
+    }
+}
